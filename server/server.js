8529c3f6fdfef0dafa42cb3dd332168a4b4aaa50
takumi <takumi.imanaka@gmail.com>
2023-07-05 22:32:50 -0700

    added api call to word api

--- server/server.js
+++ server/server.js
@@ -9,6 +9,16 @@ app.get('/', (req, res) => {
     res.send('takumi imanaka');
 });
 
+app.get('/words', (req, res) => {
+    axios.get('https://random-word-api.vercel.app/api?words=10')
+    .then(response => {
+        res.json(response.data);
+    })
+    .catch(error=>{
+        res.status(500).json({error:'an error occurred'});
+    })
+});
+
 app.listen(port, () => {
     console.log('app listening on port ${port}');
 })
